bb8c4eb83a03e8e43547a4ff4ceab101eb8d91c5
JD Tadlock <jt7903@gmail.com>
2023-09-26 13:10:34 -0400

    variables done

---
+++ index.js
@@ -0,0 +1,31 @@
+let num = 10;
+const age = 43;
+const data = {
+  name: 'JD',
+  age: 43
+};
+
+num++;
+
+data.age++;
+
+data = ['one', 'two'];
+
+console.log(data);
+
+
+
+
+
+
+
+// if (true) {
+//   let num = 'something';
+// }
+
+// if (true) {
+//   let something = 'something';
+
+//   console.log(something);
+// }
+
